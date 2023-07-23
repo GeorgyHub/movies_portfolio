b1d5c99b6f4827c1f88eeef97321825d9d30a33b
Georgy <georgy.polkanov@yandex.ru>
2023-07-23 15:08:46 +0300

    first_step

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies_app.apps.MoviesappConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'movies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'template')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "movies_app/static/movies_app",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- movies/movies_app/models.py
+++ movies/movies_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movies(models.Model):
+    class Meta:
+        verbose_name = "Фильмы"
+        verbose_name_plural = "Фильм"
+        ordering = ["-created_at"]
+
+    name = models.CharField(max_length=50, null=True, verbose_name = "Имя")
+    image = models.ImageField(upload_to='media/image/%Y/%M/%D', verbose_name = "Изображение")
+    text = models.TextField(max_length=300, null=True, verbose_name = "Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name = "Создан")
